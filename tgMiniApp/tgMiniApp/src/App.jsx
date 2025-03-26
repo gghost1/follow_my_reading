d5e07f05af5d5d3a2b388cb095d3762a0cdb86b7
samerspc <mas.norvg13@gmail.com>
2025-03-26 13:39:55 +0300

    Synchronize text and audio implemented

--- tgMiniApp/tgMiniApp/src/App.jsx
+++ tgMiniApp/tgMiniApp/src/App.jsx
@@ -11,7 +11,7 @@ function App() {
     return (
       <Routes>
         <Route path="/" element={<Home />}/>
-        {/* <Route path="/:id" element={<TextDetail />} /> */}
+        <Route path="/:id" element={<TextDetail />} />
       </Routes> 
     );
 }
